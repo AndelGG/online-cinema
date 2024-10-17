7b22394fd8b7045d0d359e49ef8102dda0698fd5
AndelGG <andolanderfgh@gmail.com>
2024-10-18 02:13:10 +0300

    add user login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { AuthDto } from './dto/auth.dto'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly AuthService: AuthService) {
-  }
-  @Post('register')
-  async register(@Body() dto:any){
-    return this.AuthService.register(dto)
-  }
+	constructor(private readonly AuthService: AuthService) {}
 
+	@HttpCode(200)
+	@UsePipes(new ValidationPipe())
+	@Post('register')
+	async register(@Body() dto: AuthDto) {
+		return this.AuthService.register(dto)
+	}
+
+	@HttpCode(200)
+	@UsePipes(new ValidationPipe())
+	@Post('login')
+	async login(@Body() dto: AuthDto) {
+		return this.AuthService.login(dto)
+	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common'
 import { InjectModel } from 'nestjs-typegoose'
 import { ModelType } from '@typegoose/typegoose/lib/types'
-import { UserModel } from 'src/user/user.model';
+import { UserModel } from 'src/user/user.model'
+import { AuthDto } from './dto/auth.dto'
+import { genSalt, compare, hash } from 'bcryptjs'
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>) {
-  }
-  async register(dto: any){
-    const newUser = new this.UserModel(dto)
+	constructor(
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+	) {}
 
-    return newUser.save()
-  }
+	async login(dto: AuthDto): Promise<UserModel> {
+		const user = await this.UserModel.findOne({ email: dto.email })
+		if (!user) throw new UnauthorizedException('User not found')
+		const isValid = await compare(dto.password, user.password)
+		if (!isValid) throw new UnauthorizedException('Incorrect password')
+		return user
+	}
 
+	async register(dto: AuthDto) {
+		const oldUser = await this.UserModel.findOne({ email: dto.email })
+		if (oldUser) throw new BadRequestException('User already exist')
+
+		const salt = await genSalt(10)
+
+		const newUser = new this.UserModel({
+			email: dto.email,
+			password: await hash(dto.password, salt),
+		})
+		return newUser.save()
+	}
 }
