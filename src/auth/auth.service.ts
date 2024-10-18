00d9e28612b2ac44f77eea5bceb8fd90d4ed5f97
AndelGG <andolanderfgh@gmail.com>
2024-10-18 13:15:45 +0300

    add tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,19 +8,23 @@ import { ModelType } from '@typegoose/typegoose/lib/types'
 import { UserModel } from 'src/user/user.model'
 import { AuthDto } from './dto/auth.dto'
 import { genSalt, compare, hash } from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
 	constructor(
-		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService
 	) {}
 
-	async login(dto: AuthDto): Promise<UserModel> {
-		const user = await this.UserModel.findOne({ email: dto.email })
-		if (!user) throw new UnauthorizedException('User not found')
-		const isValid = await compare(dto.password, user.password)
-		if (!isValid) throw new UnauthorizedException('Incorrect password')
-		return user
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto)
+		const tokens = await this.issueTokenPair(String(user._id))
+
+		return {
+			user: this.returnUserField(user),
+			...tokens,
+		}
 	}
 
 	async register(dto: AuthDto) {
@@ -33,6 +37,41 @@ export class AuthService {
 			email: dto.email,
 			password: await hash(dto.password, salt),
 		})
-		return newUser.save()
+		const tokens = await this.issueTokenPair(String(newUser._id))
+
+		return {
+			user: this.returnUserField(newUser),
+			...tokens,
+		}
+	}
+
+	async validateUser(dto: AuthDto) {
+		const user = await this.UserModel.findOne({ email: dto.email })
+		if (!user) throw new UnauthorizedException('User not found')
+		const isValid = await compare(dto.password, user.password)
+		if (!isValid) throw new UnauthorizedException('Incorrect password')
+		return user
+	}
+
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId }
+
+		const refreshToken = await this.jwtService.signAsync(data, {
+			expiresIn: '15d',
+		})
+
+		const accessToken = await this.jwtService.signAsync(data, {
+			expiresIn: '1h',
+		})
+
+		return { refreshToken, accessToken }
+	}
+
+	returnUserField(user: UserModel) {
+		return {
+			_id: user._id,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		}
 	}
 }
